9110d19647d8df2dd31314641eee692b6a313a88
Ashild Tenold Fridtun <aashiltf@stud.ntnu.no>
2023-05-02 11:19:41 +0200

    new functionality

--- app.py
+++ app.py
@@ -2,6 +2,71 @@
 from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
+
+# Incorporate data
+df = pd.read_csv('./data/movies_metadata.csv')
+
+# Initialize the app
+app = Dash(__name__)
+
+# App layout
+app.layout = html.Div([
+    html.H4('Interactive scatter plot with Iris dataset'),
+    dcc.Graph(id="scatter-plot"),
+    html.P("Filter by petal width:"),
+    dcc.RangeSlider(
+        id='range-slider',
+        min=0, max=2.5, step=0.1,
+        marks={0: '0', 2.5: '2.5'},
+        value=[0.5, 2]
+    ),
+    html.Div(children='The movies dataset'),
+    html.Hr(),
+    dcc.RadioItems(options=['popularity', 'vote_count', 'vote_average'], value='popularity', id='controls-and-radio-item'),
+    dash_table.DataTable(data=df.to_dict('records'), page_size=6),
+    dcc.Graph(figure={}, id='controls-and-graph')
+])
+
+@app.callback(
+    Output("scatter-plot", "figure"), 
+    Input("range-slider", "value"))
+def update_bar_chart(slider_range):
+    df = px.data.iris() # replace with your own data source
+    low, high = slider_range
+    mask = (df['petal_width'] > low) & (df['petal_width'] < high)
+    fig = px.scatter(
+        df[mask], x="sepal_width", y="sepal_length", 
+        color="species", size='petal_length', 
+        hover_data=['petal_width'])
+    return fig
+# Add controls to build the interaction
+@callback(
+    Output(component_id='controls-and-graph', component_property='figure'),
+    Input(component_id='controls-and-radio-item', component_property='value')
+)
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='title', y=col_chosen, histfunc='avg')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+"""
+# Import packages
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
+import pandas as pd
+import plotly.express as px
 import dash_bootstrap_components as dbc
 
 # Incorporate data
@@ -14,10 +79,34 @@ external_stylesheets = [dbc.themes.CERULEAN]
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 
+
+# App layout
+app.layout = html.Div([
+    html.Div(className='row',children='My First App with Data, Graph, and Controls', style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+    html.Hr(),
+    html.Div(className='row', children=[dcc.RadioItems(options=['popularity', 'vote_count', 'vote_average'], value='popularity',inline=True, id='my-radio-buttons-final')]),
+    html.Div(className='row', children=[dash_table.DataTable(data=new_df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})]),
+    html.Div(className='six columns', children=[  dcc.Graph(figure={}, id='histo-chart-final')])
+  
+])
+@callback(
+    Output(component_id='histo-chart-final', component_property='figure'),
+    Input(component_id='my-radio-buttons-final', component_property='value')
+)
+
+def update_graph(col_chosen):
+    fig = px.histogram(df, x='title', y=col_chosen, histfunc='avg')
+    return fig
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
 # App layout
 app.layout = dbc.Container([
     dbc.Row([
-        html.Div('My First App with Data, Graph, and Controls', className="text-primary text-center fs-3")
+        html.Div('The movies dataset', className="text-primary text-center fs-3")
     ]),
 
     dbc.Row([
@@ -51,33 +140,4 @@ def update_graph(col_chosen):
 # Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-"""
-
-# App layout
-app.layout = html.Div([
-    html.Div(className='row',children='My First App with Data, Graph, and Controls', style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
-    html.Hr(),
-    html.Div(className='row', children=[dcc.RadioItems(options=['popularity', 'vote_count', 'vote_average'], value='popularity',inline=True, id='my-radio-buttons-final')]),
-    html.Div(className='row', children=[dash_table.DataTable(data=new_df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})]),
-    html.Div(className='six columns', children=[  dcc.Graph(figure={}, id='histo-chart-final')])
-  
-
-
-    #dash_table.DataTable(data=new_df.to_dict('records'), page_size=10),
-    #dcc.Graph(figure=px.histogram(new_df, x='title', y='popularity', histfunc='avg'))
-])
-@callback(
-    Output(component_id='histo-chart-final', component_property='figure'),
-    Input(component_id='my-radio-buttons-final', component_property='value')
-)
-
-def update_graph(col_chosen):
-    fig = px.histogram(df, x='title', y=col_chosen, histfunc='avg')
-    return fig
-
-# Run the app
-if __name__ == '__main__':
-    app.run_server(debug=True)
-
 """
